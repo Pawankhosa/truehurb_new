229cf15ec322898e100d2f29ac287965a04d886e
Pawankhosa <32534053+Pawankhosa@users.noreply.github.com>
2018-08-18 17:10:30 +0530

    changes

--- TrueHerb/User/Saleproduct.aspx.cs
+++ TrueHerb/User/Saleproduct.aspx.cs
@@ -12,13 +12,14 @@ public partial class User_Saleproduct : System.Web.UI.Page
 {
     SQLHelper objsql = new SQLHelper();
     public DataTable MyDT = new DataTable();
+    public DataTable MyTABLE = new DataTable();
     DataRow MyRow;
     string constring = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
     int id = 0, total = 0, qty2 = 0, pvpr = 0;
     public static string invoice = "", check = "", code = "",user;
     public static int cnt = 0;
     Common cm = new Common();
-    public static string date = "";
+    public static string date = "", qnnty = "0";
     private static TimeZoneInfo INDIAN_ZONE;
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -142,91 +143,67 @@ public partial class User_Saleproduct : System.Web.UI.Page
         cnt = 0;
     }
 
-    public void checkqty()
-    {
-        code = Common.Get(objsql.GetSingleValue("select code from tblproducts where name='" + txtname.Text + "'"));
-        check = Common.Get(objsql.GetSingleValue("select stock from tblAssignstock where code='" + code + "' and regno='" + Session["user"].ToString() + "'"));
-        if (Convert.ToInt32(check) < Convert.ToInt32(txtqty.Text))
-        {
-            Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", "alert('Only " + check + " stock is left');", true);
-            // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Only " + check + " stock is left')", true);
-        }
-    }
+    //public void checkqty()
+    //{
+    //    code = Common.Get(objsql.GetSingleValue("select code from tblproducts where name='" + txtname.Text + "'"));
+    //    check = Common.Get(objsql.GetSingleValue("select stock from tblAssignstock where code='" + code + "' and regno='" + Session["user"].ToString() + "'"));
+    //    if (Convert.ToInt32(check) < Convert.ToInt32(txtqty.Text))
+    //    {
+    //        Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", "alert('Only " + check + " stock is left');", true);
+    //        // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Only " + check + " stock is left')", true);
+    //    }
+    //}
 
     protected void btnsubmit_Click1(object sender, EventArgs e)
     {
-        checkqty();
-        if (Convert.ToInt32(check) >= Convert.ToInt32(txtqty.Text))
+        code = Common.Get(objsql.GetSingleValue("select code from tblproducts where name='" + txtname.Text + "'"));
+        check = Common.Get(objsql.GetSingleValue("select stock from tblAssignstock where code='" + code + "' and regno='" + Session["user"].ToString() + "'"));
+        if (cnt != 0)
         {
-           
-            if(cnt==0)
-            { 
-            //if (Session["DataTable"] == "" && Session["DataTable"]!=null)
+            MyTABLE = (DataTable)Session["DataTable"];
+          
+            foreach (DataRow dtrow in MyTABLE.Rows)
+            {
+                if (code == dtrow["Code"].ToString())
+                {
+                    qnnty = ((Convert.ToInt32(dtrow["Quantity"].ToString())) + (Convert.ToInt32(txtqty.Text))).ToString();
+                }
+            }
+         }
+        else
+        {
+            qnnty = txtqty.Text;
+        }
+        if (Convert.ToInt32(check) < Convert.ToInt32(qnnty))
+        {
+            txtqty.Text = "";
+            qnnty = "0";
+            Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", "alert('Only " + check + " stock is left');", true);
+            // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Only " + check + " stock is left')", true);
+        }
+        else
+        {
+            //if (Convert.ToInt32(check) >= Convert.ToInt32(txtqty.Text))
             //{
 
-                MyDT.Columns.Add("id", System.Type.GetType("System.Int32"));
-
-                MyDT.Columns.Add("Name");
-
-                MyDT.Columns.Add("Quantity");
-                MyDT.Columns.Add("Price");
-                MyDT.Columns.Add("PV");
-                MyDT.Columns.Add("Total");
-                MyDT.Columns.Add("PVTotal");
-                MyDT.Columns.Add("pid");
-                MyDT.Columns.Add("Code");
-                MyDT.Columns.Add();
-
-                MyRow = MyDT.NewRow();
-
-                MyRow[0] = MyDT.Rows.Count + 1;
-
-                MyRow[1] = txtname.Text;
-
-                MyRow[2] = txtqty.Text;
-                MyRow[3] = Common.Get(objsql.GetSingleValue("select Dp from tblproducts where code='" + code + "'"));
-                MyRow[4] = Common.Get(objsql.GetSingleValue("select pv from tblproducts where code='" + code + "'"));
-                MyRow[5] = (Convert.ToInt32(MyRow[3]) * Convert.ToInt32(MyRow[2]));
-                MyRow[6] = (Convert.ToInt32(MyRow[4]) * Convert.ToInt32(MyRow[2]));
-                MyRow[7] = Common.Get(objsql.GetSingleValue("select id from tblproducts where code='" + code + "'"));
-                MyRow[8] = code;
-                MyDT.Rows.Add(MyRow);
-
-
-               Session["DataTable"] = MyDT;
-                cnt += 1;
-
-            }
-           
-            else
-            {
-                MyDT = (DataTable)Session["DataTable"];
-                string pccode=null;
-                foreach(DataRow dtr in MyDT.Rows)
+                if (cnt == 0)
                 {
-                    if(code== dtr["Code"].ToString())
-                    {
-                        pccode = "already";
-                        int qt = (Convert.ToInt32(MyDT.Rows[0][2].ToString()) + Convert.ToInt32(txtqty.Text));
+                    //if (Session["DataTable"] == "" && Session["DataTable"]!=null)
+                    //{
 
-                        DataRow dr = dtr;
-                        dr["Quantity"] = qt.ToString();
-                        dr["Price"] = Common.Get(objsql.GetSingleValue("select Dp from tblproducts where code='" + code + "'"));
-                        dr["PV"] = Common.Get(objsql.GetSingleValue("select pv from tblproducts where code='" + code + "'"));
-                        dr["Total"] = (Convert.ToInt32(dr["Price"].ToString()) * Convert.ToInt32(qt));
-                        dr["PVTotal"] = (Convert.ToInt32(dr["PV"].ToString()) * Convert.ToInt32(qt));
-                        MyDT.AcceptChanges();
-                    }
-                }
-                
-                if (pccode != null)
-                {
+                    MyDT.Columns.Add("id", System.Type.GetType("System.Int32"));
 
+                    MyDT.Columns.Add("Name");
 
-                }
-                else
-                {
-                    MyDT = (DataTable)Session["DataTable"];
+                    MyDT.Columns.Add("Quantity");
+                    MyDT.Columns.Add("Price");
+                    MyDT.Columns.Add("PV");
+                    MyDT.Columns.Add("Total");
+                    MyDT.Columns.Add("PVTotal");
+                    MyDT.Columns.Add("pid");
+                    MyDT.Columns.Add("Code");
+                    MyDT.Columns.Add();
+
                     MyRow = MyDT.NewRow();
 
                     MyRow[0] = MyDT.Rows.Count + 1;
@@ -241,22 +218,74 @@ public partial class User_Saleproduct : System.Web.UI.Page
                     MyRow[7] = Common.Get(objsql.GetSingleValue("select id from tblproducts where code='" + code + "'"));
                     MyRow[8] = code;
                     MyDT.Rows.Add(MyRow);
+
+
+                    Session["DataTable"] = MyDT;
+                    cnt += 1;
+
                 }
 
-                Session["DataTable"] = MyDT;
-            }
-            GridView1.DataSource = MyDT;
-            GridView1.DataBind();
-            bindtotal();
-            Button1.Visible = true;
-            txtname.Text = "";
-            txtqty.Text = "";
+                else
+                {
+                    MyDT = (DataTable)Session["DataTable"];
+                    string pccode = null;
+                    foreach (DataRow dtr in MyDT.Rows)
+                    {
+                        if (code == dtr["Code"].ToString())
+                        {
+                            pccode = "already";
+                            int qt = (Convert.ToInt32(MyDT.Rows[0][2].ToString()) + Convert.ToInt32(txtqty.Text));
 
-        }
-        else
-        {
-            Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", "alert('Only " + check + " stock is left');", true);
-            //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Only " + check + " stock is left')", true);
-        }
+                            DataRow dr = dtr;
+                            dr["Quantity"] = qt.ToString();
+                            dr["Price"] = Common.Get(objsql.GetSingleValue("select Dp from tblproducts where code='" + code + "'"));
+                            dr["PV"] = Common.Get(objsql.GetSingleValue("select pv from tblproducts where code='" + code + "'"));
+                            dr["Total"] = (Convert.ToInt32(dr["Price"].ToString()) * Convert.ToInt32(qt));
+                            dr["PVTotal"] = (Convert.ToInt32(dr["PV"].ToString()) * Convert.ToInt32(qt));
+                            MyDT.AcceptChanges();
+                        }
+                    }
+
+                    if (pccode != null)
+                    {
+
+
+                    }
+                    else
+                    {
+                        MyDT = (DataTable)Session["DataTable"];
+                        MyRow = MyDT.NewRow();
+
+                        MyRow[0] = MyDT.Rows.Count + 1;
+
+                        MyRow[1] = txtname.Text;
+
+                        MyRow[2] = txtqty.Text;
+                        MyRow[3] = Common.Get(objsql.GetSingleValue("select Dp from tblproducts where code='" + code + "'"));
+                        MyRow[4] = Common.Get(objsql.GetSingleValue("select pv from tblproducts where code='" + code + "'"));
+                        MyRow[5] = (Convert.ToInt32(MyRow[3]) * Convert.ToInt32(MyRow[2]));
+                        MyRow[6] = (Convert.ToInt32(MyRow[4]) * Convert.ToInt32(MyRow[2]));
+                        MyRow[7] = Common.Get(objsql.GetSingleValue("select id from tblproducts where code='" + code + "'"));
+                        MyRow[8] = code;
+                        MyDT.Rows.Add(MyRow);
+                    }
+
+                    Session["DataTable"] = MyDT;
+                }
+                GridView1.DataSource = MyDT;
+                GridView1.DataBind();
+                bindtotal();
+                Button1.Visible = true;
+                txtname.Text = "";
+                txtqty.Text = "";
+
+            }
+        
+            //else
+            //{
+            //    Page.ClientScript.RegisterStartupScript(GetType(), "msgbox", "alert('Only " + check + " stock is left');", true);
+            //    //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Only " + check + " stock is left')", true);
+            //}
+       
     }
 }
